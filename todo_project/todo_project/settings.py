3c9cd58f6b815e0a0c400375a389914da23eb36c
shroukhegazi <shroukhegazi99@gmail.com>
2022-02-19 21:44:25 +0200

    move app directory  to the root directory  of the project

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -31,16 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'todos.apps.TodosConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos',
+    
     
     
 
+    
 ]
 
 MIDDLEWARE = [
